96d79f2a6a55a267cff4a71bbe21e062495751b7
Matt Brown <matthewtbrown124@gmail.com>
2021-04-15 13:13:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+ return (
+   fetch('https://anapioficeandfire.com/api/books')
+   .then((response) => response.json())
+   .then((json) => renderBooks(json))
+ )
   
 }
 
@@ -13,5 +17,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  renderBooks();
 });
